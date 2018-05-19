ea9f1a9616d522e05691ddf920cf436ec0f0da93
Jhonny <killingjokewow@gmail.com>
2018-05-19 10:49:19 -0600

    Correct Format

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,22 +1,22 @@
 class Url < ApplicationRecord
 
-	validates_presence_of :url, :access_count
-	validates_format_of :url, with:  /\A(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
-	scope :top, -> (order, limit) { order(access_count: order).limit(limit) }
+  validates_presence_of :url, :access_count
+  validates_format_of :url, with:  /\A(?:(?:http|https):\/\/)?([-a-zA-Z0-9.]{2,256}\.[a-z]{2,4})\b(?:\/[-a-zA-Z0-9@,!:%_\+.~#?&\/\/=]*)?\z/
+  scope :top, -> (order, limit) { order(access_count: order).limit(limit) }
 
-	before_create :generate_short_url
+  before_create :generate_short_url
   after_create :set_access
 
-	def generate_short_url
-		chars = ['0'..'9','A'..'Z','a'..'z'].map{|range| range.to_a}.flatten
-		# here we assign a short_url
-		self.short_url = 6.times.map{chars.sample}.join
-		# to check if the short url already exist
-		self.short_url = 6.times.map{chars.sample}.join until Url.find_by_short_url(self.short_url).nil?
-	end
+  def generate_short_url
+    chars = ['0'..'9','A'..'Z','a'..'z'].map{|range| range.to_a}.flatten
+    # here we assign a short_url
+    self.short_url = 6.times.map{chars.sample}.join
+    # to check if the short url already exist
+    self.short_url = 6.times.map{chars.sample}.join until Url.find_by_short_url(self.short_url).nil?
+  end
 
-	def find_duplicate
-		u = Url.find_by_sanitize_url(self.sanitize_url)
+  def find_duplicate
+    u = Url.find_by_sanitize_url(self.sanitize_url)
   end
 
   def new_url?
